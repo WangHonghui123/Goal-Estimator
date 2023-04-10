c72555cffc49827a614fc5c2ae1bdfcd2544171f
WangHonghui123 <349638414@qq.com>
2023-04-10 13:46:25 +1000

    Plot the Goal Estimation for ETH/UCY Dataset

---
+++ plot.py
@@ -0,0 +1,19 @@
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+open_directory = "./goal_estimated_result/ETH_UCY/"
+dataset = ['univ', 'eth', 'zara1', 'zara2', 'hotel']
+for dataname in dataset:
+    open_error_file = open_directory + "goal_estimated_error_" + dataname + "_2.pkl"
+    open_estimation_file = open_directory + "goal_estimated_" + dataname + "_2.pkl"
+    error_file = open(open_error_file,'rb')
+    error = pickle.load(error_file)
+    error = np.concatenate(error['Estimated_Goal_Error'])
+    aver_error = sum(error) / len(error)
+    pedestrian = np.arange(0,len(error))
+    plt.hist(error, density=True)
+    plt.xlabel('L2-norm error')
+    plt.ylabel('The frequency of L2-norm error')
+    plt.title(f'The goal estimation on {dataname} \n with average L2-norm loss {aver_error}')
+    plt.show()
